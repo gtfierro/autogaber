dae8ce35ec2818522397328b4eb4f19d3fd24fc0
Gabe Fierro <gtfierro225@gmail.com>
2014-02-04 21:18:00 -0500

    expose flask app and limit content to 16MB

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # limit max upload to 16 MB
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0',debug=True)
